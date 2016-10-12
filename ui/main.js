bc67cd021c04380e35eb0f772d48a3e7cec745e6
abhijithragh <abhijith.ragh@gmail.com>
2016-10-12 16:27:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = ':)';
 
-var img = document.getElementById('madi');
+//var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
     {
     marginLeft+=1;
-    img.style.marginLeft=marginLeft+'px';
+    madi.style.marginLeft=marginLeft+'px';
     }
-img.onclick = function()
+madi.onclick = function()
     {
     var interval = setInterval(moveRight,50);
     };
