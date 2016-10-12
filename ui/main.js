faa79a3e7ee2d08d7ed877f974e16cf27f45e00c
abhijithragh <abhijith.ragh@gmail.com>
2016-10-12 16:20:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,15 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML = ':)';
 
+var img = document.getEementById('madi');
+var marginLeft=0;
+function moveRight()
+    {
+    marginLeft+=1;
+    img.style.marginLeft+='px';
+    }
+img.onclick = function()
+    {
+    var interval = setInterval(moveRight,50);
+    };
+
